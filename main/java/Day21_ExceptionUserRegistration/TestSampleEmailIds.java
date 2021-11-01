63c96751a87da1eb70b6dd4716f6c308ea11d89a
Afroj Satwilkar <afrozsatvilkar2014@gmail.com>
2021-11-01 16:30:28 +0530

    Test sample email ids

---
+++ main/java/Day21_ExceptionUserRegistration/TestSampleEmailIds.java
@@ -0,0 +1,13 @@
+package Day21_ExceptionUserRegistration;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TestSampleEmailIds {
+
+    public boolean testForValidEmails(String validEmails) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z-9]+([._+-]*[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$");
+        Matcher matcher = pattern.matcher(validEmails);
+        return matcher.matches();
+    }
+}
